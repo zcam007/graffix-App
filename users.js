cbc2659a35e49c515a35497b1ed19b12e738fd20
Chandu <chandu.prince951@gmail.com>
2019-05-28 16:06:52 -0700

    Added Semester control

--- users.js
+++ users.js
@@ -2,3 +2,16 @@ var btn=document.getElementById('goBackBtn');
 btn.addEventListener('click',function (){
   document.location.href="index.html";
 });
+
+if(document.querySelector('#semesterDropdown')!=null)
+{
+    var x = document.getElementById("semesterDropdown");
+    var semesters=["Fall","Spring","Summer"];
+    for(var i=0;i<semesters.length;i++)
+    {
+        var option = document.createElement("option");
+        option.text = semesters[i];
+        option.value=semesters[i];
+        x.add(option);
+    }
+}
