6987f7b5477363ea3c6f552176958f31d1ce6fc0
Chandu <chandu.prince951@gmail.com>
2019-06-27 16:14:38 -0700

    Analysis table alpha pre release complete

--- analysis.js
+++ analysis.js
@@ -65,6 +65,47 @@ const loadAnalysisTable=()=>{
 }
 
 
+
+
+
+
+
+// const getCountMatrix=(semester)=>{
+// firebase.database().ref('/'+semester).once('value').then(function(snapshot) {
+//     var jsonArr=snapshotToArray(snapshot);
+//    // console.log(jsonArr);
+// console.log(getDeptCount(jsonArr,"ccc"));
+// console.log(getCAMcount(jsonArr,"ccc"));
+// console.log(getShirtCount(jsonArr,"ccc"));
+// console.log(getWebCount(jsonArr,"ccc"));
+// console.log(getCancelCount(jsonArr,"ccc"));
+// console.log(getCAM_SCount(jsonArr,"ccc"));
+// //let matrix=[[]];
+// var matrix = Create2DArray(8);
+// var departments=["ccc","csi","the pit","xtreme","operation","csula","u-su","graffix"]
+// for(var i=0;i<departments.length;i++){
+// let deptCount=getDeptCount(jsonArr,departments[i]);
+// let camCount=getCAMcount(jsonArr,departments[i]);
+// let cam_sCount=getCAM_SCount(jsonArr,departments[i])
+// let shirtCount=getShirtCount(jsonArr,departments[i]);
+// let webCount=getWebCount(jsonArr,departments[i])
+// let cancelCount=getCancelCount(jsonArr,departments[i]);
+// let themeCount=deptCount-(camCount+cam_sCount+shirtCount+webCount);
+
+// matrix[i][0]=camCount;
+// matrix[i][1]=cam_sCount;
+// matrix[i][2]=themeCount;
+// matrix[i][3]=shirtCount;
+// matrix[i][4]=webCount;
+// matrix[i][5]=cancelCount;
+
+// }
+// console.log(matrix);
+// return matrix;
+// });
+// }
+
+
 const newtable=(semsesterName)=>{
     var table=document.createElement('table');
     var th=document.createElement('th');
@@ -91,13 +132,59 @@ const newtable=(semsesterName)=>{
     var tr=[]
     var td=[]
     
+    //let matrixComputed=getCountMatrix(semsesterName);
+
+
+    firebase.database().ref('/'+semsesterName).once('value').then(function(snapshot) {
+        var jsonArr=snapshotToArray(snapshot);
+       // console.log(jsonArr);
+    console.log(getDeptCount(jsonArr,"ccc"));
+    console.log(getCAMcount(jsonArr,"ccc"));
+    console.log(getShirtCount(jsonArr,"ccc"));
+    console.log(getWebCount(jsonArr,"ccc"));
+    console.log(getCancelCount(jsonArr,"ccc"));
+    console.log(getCAM_SCount(jsonArr,"ccc"));
+    //let matrix=[[]];
+    var matrix = Create2DArray(8);
+    var departments=["ccc","csi","the pit","xtreme","operations","csula","u-su","graffix"]
+    var rowCount=0;
+    for(var i=0;i<departments.length;i++){
+    let deptCount=getDeptCount(jsonArr,departments[i]);
+    let camCount=getCAMcount(jsonArr,departments[i]);
+    let cam_sCount=getCAM_SCount(jsonArr,departments[i])
+    let shirtCount=getShirtCount(jsonArr,departments[i]);
+    let webCount=getWebCount(jsonArr,departments[i])
+    let cancelCount=getCancelCount(jsonArr,departments[i]);
+    let themeCount=deptCount-(camCount+cam_sCount+shirtCount+webCount);
+    
+    matrix[i][0]=camCount;
+    matrix[i][1]=cam_sCount;
+    matrix[i][2]=themeCount;
+    matrix[i][3]=shirtCount;
+    matrix[i][4]=webCount;
+    matrix[i][5]=cancelCount;
+    rowCount=i;
+    }
+    console.log(matrix);
+    let columnSum=columnSumOfMatrix(matrix);
+    matrix[rowCount+1]=columnSum;
+    let totalCount=0;
+    
+    console.log(columnSum);
+    for(var i=0;i<columnSum.length-1;i++){
+        totalCount+=columnSum[i];
+    }
+
     for(var i=0;i<9;i++)
     {
         tr[i]=document.createElement('tr');
         for(var j=0;j<6;j++)
         {
             td[j]=document.createElement('td');
-            var text=document.createTextNode(" ");
+            if(matrix[i][j]!=0){
+            var text=document.createTextNode(matrix[i][j]);
+            }
+            else var text=document.createTextNode("");
             td[j].appendChild(text);
             tr[i].appendChild(td[j]);
         }
@@ -105,49 +192,78 @@ const newtable=(semsesterName)=>{
     }
     
     document.getElementById("tablesDiv").appendChild(table);
+
+    var totalTR=document.createElement('tr');
+    var totalTD=document.createElement('td');
+    var text1=document.createTextNode(totalCount);
+    totalTD.setAttribute("colspan", "6");
+    totalTD.setAttribute("class", "semesterTotalCount");
+    totalTD.appendChild(text1);
+    totalTR.appendChild(totalTD);
+    table.appendChild(totalTR);
+    createTotalTable();
+    });   
+    
+}
+
+
+const calcGrandTotal=()=>{
+    var sum = 0;
+$('.semesterTotalCount').each(function()
+{
+    sum += parseInt($(this).text());
+});
+return sum;
+}
+
+const createTotalTable=()=>
+{
+    var table=document.createElement('table');
+    table.setAttribute("id","totalTable");
+    table.setAttribute("class","totalTable");
+    table.setAttribute("style","display:none");
+    var th=document.createElement('th');
+    var tr=document.createElement('tr');
+    var text=document.createTextNode("Total");
+    th.appendChild(text);
+    //th.setAttribute("colspan", "6");
+    tr.appendChild(th);
+    table.appendChild(tr);
+
+    var tr1=document.createElement('tr');
+    var td=document.createElement('td');
+    
+    td.setAttribute("rowspan","0");
+    td.setAttribute("style","height:539px");
+    td.setAttribute("id","totalValue")
+    text=document.createTextNode(calcGrandTotal());
+    td.appendChild(text);
+    tr1.appendChild(td);
+    table.appendChild(tr1);
+    
+    document.getElementById("tablesDiv").appendChild(table);
+    calcGrandTotal();
+}
+
+
+const columnSumOfMatrix=(arr)=>{
+    var res = [];
+    for(var i=0;i<arr.length;i++){
+        for(var j=0;j<arr[i].length;j++){
+         res[j] = (res[j] || 0) + arr[i][j];
+        }
+       }
+      // console.log(res);
+       return res;
 }
 
 if(document.querySelector('#analysisTable')!=null){
   
-//loadAnalysisTable();
-newtable("Summer");
-newtable("Fall");
-
-}
-
-firebase.database().ref('/Summer 2019').once('value').then(function(snapshot) {
-    var jsonArr=snapshotToArray(snapshot);
-    //console.log(jsonArr);
-console.log(getDeptCount(jsonArr,"ccc"));
-console.log(getCAMcount(jsonArr,"ccc"));
-console.log(getShirtCount(jsonArr,"ccc"));
-console.log(getWebCount(jsonArr,"operations"));
-console.log(getCancelCount(jsonArr));
-console.log(getCAM_SCount(jsonArr,"operations"));
-//let matrix=[[]];
-var matrix = Create2DArray(8);
-var departments=["ccc","csi","the pit","xtreme","operation","csula","u-su","graffix"]
-for(var i=0;i<departments.length;i++){
-let deptCount=getDeptCount(jsonArr,departments[i]);
-let camCount=getCAMcount(jsonArr,departments[i]);
-let cam_sCount=getCAM_SCount(jsonArr,departments[i])
-let shirtCount=getShirtCount(jsonArr,departments[i]);
-let webCount=getWebCount(jsonArr,departments[i])
-let cancelCount=getCancelCount(jsonArr,departments[i]);
-let themeCount=deptCount-(camCount+cam_sCount+shirtCount+webCount);
-
-matrix[i][0]=camCount;
-matrix[i][1]=cam_sCount;
-matrix[i][2]=themeCount;
-matrix[i][3]=shirtCount;
-matrix[i][4]=webCount;
-matrix[i][5]=cancelCount;
-
-}
-console.log(matrix);
-
-});
-
+    //loadAnalysisTable();
+    newtable("Summer 2019");
+    newtable("Fall 2018");
+    //createTotalTable();
+    }
 // get count functions
 
 
