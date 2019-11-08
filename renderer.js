1f60a0dd81eb7f2519e9e438bc3953c641ce6ec8
Chandu <chandu.prince951@gmail.com>
2019-11-08 15:27:34 -0800

    CDN test

--- renderer.js
+++ renderer.js
@@ -9,7 +9,7 @@ if(appVObj!=null){
 appVObj.innerHTML="App Version:"+appVersion;
 }
 
-console.log("loaded from web")
+console.log("loaded from web--test")
 
 //imports
 var firebase=require('./firebase/firebase.js');
